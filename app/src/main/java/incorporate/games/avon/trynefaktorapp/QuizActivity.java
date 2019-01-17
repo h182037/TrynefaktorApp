a6bf597288da2edb4a412833096eff91150bbc6d
h182037 <sindre.aalhus@gmail.com>
2019-01-17 11:25:46 +0100

    initial

---
+++ app/src/main/java/incorporate/games/avon/trynefaktorapp/QuizActivity.java
@@ -0,0 +1,13 @@
+package incorporate.games.avon.trynefaktorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
