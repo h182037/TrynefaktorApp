58abeeb56ece45637b528fc15fb11d1af396261c
h182037 <sindre.aalhus@gmail.com>
2019-01-17 12:00:57 +0100

    initial

--- app/src/main/java/incorporate/games/avon/trynefaktorapp/MainActivity.java
+++ app/src/main/java/incorporate/games/avon/trynefaktorapp/MainActivity.java
@@ -1,13 +1,26 @@
 package incorporate.games.avon.trynefaktorapp;
 
+import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToQuiz(View v){
+        Intent intent = new Intent(MainActivity.this,QuizActivity.class);
+        startActivity(intent);
+    }
+    public void goToPlayers(View v){
+        Intent intent = new Intent(MainActivity.this,PlayersActivity.class);
+        startActivity(intent);
     }
 }
