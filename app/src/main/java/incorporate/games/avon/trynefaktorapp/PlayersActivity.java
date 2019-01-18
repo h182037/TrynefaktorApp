544425a6e4e92bf3099ec7f81ab65ccd3d843787
h182037 <sindre.aalhus@gmail.com>
2019-01-18 10:43:12 +0100

    team

--- app/src/main/java/incorporate/games/avon/trynefaktorapp/PlayersActivity.java
+++ app/src/main/java/incorporate/games/avon/trynefaktorapp/PlayersActivity.java
@@ -1,7 +1,9 @@
 package incorporate.games.avon.trynefaktorapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PlayersActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class PlayersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players);
+    }
+
+    public void goToAdd(View v){
+        Intent intent = new Intent(PlayersActivity.this, AddActivity.class);
+        startActivity(intent);
     }
 }
